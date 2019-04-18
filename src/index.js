418abde15e21f79880596d29275ee4ba3b9b1909
Priestch <priestch@gmail.com>
2019-04-18 16:27:34 +0800

    chore: bump version

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // @name         Gitlab Issues Track
 // @namespace    http://tampermonkey.net/
 // @homepage     https://github.com/Priestch/savior
-// @version      0.1.2
+// @version      0.2.0
 // @description  Savior of bug track in Gitlab issue!
 // @author       Priestch
 // @match        https://gitpd.paodingai.com/*/issues/*
