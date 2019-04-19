09821c41ccf2725d43f9c07d523fbcf2021f73e6
Priestch <priestch@gmail.com>
2019-04-19 15:10:51 +0800

    feat: use default sort when export csv file

--- src/index.js
+++ src/index.js
@@ -168,15 +168,6 @@
         continue
       }
     }
-    tasks.sort(function (a, b) {
-      if (a.priority < b.priority) {
-        return -1;
-      }
-      if (a.priority > b.priority) {
-        return 1;
-      }
-      return 0;
-    });
     return tasks;
   }
 
