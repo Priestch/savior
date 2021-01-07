6b6afff31c168a631c30f39fd7457d248f02d587
Priestch <priestch@gmail.com>
2021-01-07 23:27:14 +0800

    chore: bump version to 0.2.8

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // @name         Gitlab Issues Track
 // @namespace    http://tampermonkey.net/
 // @homepage     https://github.com/Priestch/savior
-// @version      0.2.7
+// @version      0.2.8
 // @description  Savior of bug track in Gitlab issue!
 // @author       Priestch
 // @match        https://gitpd.paodingai.com/*/issues/*
