638788b33d3a940a839fd36baf7bfcde0ce3e07e
Priestch <priestch@gmail.com>
2021-09-08 11:31:31 +0800

    bump version 0.3.12

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // @name         Gitlab Issues Track
 // @namespace    http://tampermonkey.net/
 // @homepage     https://github.com/Priestch/savior
-// @version      0.3.11
+// @version      0.3.12
 // @description  Savior of bug track in Gitlab issue!
 // @author       Priestch
 // @match        https://gitpd.paodingai.com/*/issues/*
